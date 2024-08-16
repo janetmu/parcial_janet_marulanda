835b40290e328f52a94a74cdf7be6523c3692086
janet_marulanda@yahoo.com <janet_marulanda@yahoo.com>
2024-08-15 20:55:04 -0500

    Entrega finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
 import Card from "./Components/Card";
 
 function App() {
+  const [formData, setFormData] = useState({ title: "", author: "" });
+  const [error, setError] = useState("");
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError("");
+
+    if (formData.title.trim().length < 3 || formData.title[0] === "") {
+      setError("Por favor chequea que la información sea correcta");
+      return;
+    }
+
+    if (formData.author.length < 6) {
+      setError("Por favor chequea que la información sea correcta");
+      return;
+    }
+
+    setSubmittedData(formData);
+    setError("");
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Busqueda de libros</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Título"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="author"
+          placeholder="Autor"
+          value={formData.author}
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Enviar</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {submittedData && <Card data={submittedData} />}
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,21 @@
-import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+
+
+
+const Card = ({ data }) => {
+  return (
+    <div
+      style={{ border: "1px solid #ccc", padding: "20px", marginTop: "20px" }}
+    >
+      <h2>Información Libro</h2>
+      <p>
+        <strong>Título</strong> {data.title}
+      </p>
+      <p>
+        <strong>Autor</strong> {data.author}
+      </p>
+    </div>
+  );
 };
 
 export default Card;
